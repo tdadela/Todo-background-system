b8419f0e312ea4a44013454ba8d715383a417bad
td236 <dadela.tomasz@gmail.com>
2021-11-01 21:46:45 +0100

    install_requirements.sh: yum added

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,6 +2,7 @@
 packages='python3 texlive poppler-utils feh'
 if   [ -x "$(command -v apt)" ];    then sudo apt install $packages
 elif [ -x "$(command -v dnf)" ];    then sudo dnf install $packages
+elif [ -x "$(command -v yum)" ];    then sudo yum install $packages
 elif [ -x "$(command -v apk)" ];    then sudo apk add --no-cache $packages
 elif [ -x "$(command -v zypper)" ]; then sudo zypper install $packages
 elif [ -x "$(command -v pacman)" ]; then sudo pacman -S poppler python3 texlive feh
