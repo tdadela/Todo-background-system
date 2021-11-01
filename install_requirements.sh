35c1ed0b7ddb6575a206fb917c8f1f009bc1c820
td236 <dadela.tomasz@gmail.com>
2021-11-01 21:43:53 +0100

    install_requirements.sh added

---
+++ install_requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+packages='python3 texlive poppler-utils feh'
+if   [ -x "$(command -v apt)" ];    then sudo apt install $packages
+elif [ -x "$(command -v dnf)" ];    then sudo dnf install $packages
+elif [ -x "$(command -v apk)" ];    then sudo apk add --no-cache $packages
+elif [ -x "$(command -v zypper)" ]; then sudo zypper install $packages
+elif [ -x "$(command -v pacman)" ]; then sudo pacman -S poppler python3 texlive feh
+else echo "You need to install: $packages manually." >&2
+fi
