3917393ad100d89b97abfb112f59fac49746b370
td236 <dadela.tomasz@gmail.com>
2021-11-02 19:12:41 +0100

    fix: tasks.tex when no task

--- generate_tex_input.py
+++ generate_tex_input.py
@@ -7,16 +7,16 @@ con = sl.connect(settings.DATABASE_PATH)
 f = open(settings.TASK_TEX_FILE, 'w')
 
 with con:
-    data = con.execute('SELECT name FROM TASK')
-    if data != []:
+    data = con.execute('SELECT name FROM TASK').fetchall()
+    if len(data) != 0:
         f.write(r'Todo:\\' + '\n')
         f.write(r'\begin{enumerate}' + '\n')
         for row in data:
             f.write('\t\item ' + str(row[0]) + '\n')
-        f.write('\end{enumerate}')
+        f.write('\end{enumerate}\n')
 
-    data = con.execute('SELECT name FROM DONE')
-    if data != []:
+    data = con.execute('SELECT name FROM DONE').fetchall()
+    if len(data) != 0:
         f.write(r'Done:\\' + '\n')
         f.write(r'\begin{itemize}' + '\n')
         for row in data:
