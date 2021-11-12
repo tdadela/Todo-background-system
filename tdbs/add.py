8a068b4f5bb9cf004a032eef178a56e9fb8f90b8
td236 <dadela.tomasz@gmail.com>
2021-11-12 21:34:30 +0100

    feat: add many tasks at once

--- tdbs/add.py
+++ tdbs/add.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-
+'''Add task with given name.'''
 import sys
 import settings
 from db_operation import add_to_task
 
 if len(sys.argv) == 1:
-   exit(0) 
+    sys.exit(0)
 
-task_name = ' '.join(sys.argv[1:])
+task_names = ' '.join(sys.argv[1:]).split('.')
 
-add_to_task(settings.DATABASE_PATH, task_name)
+for task in task_names:
+    if task.strip():
+        add_to_task(settings.DATABASE_PATH, task.strip())
