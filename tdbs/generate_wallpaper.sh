721e169f7fd725e1649075b4a0798eb27ab1812b
td236 <dadela.tomasz@gmail.com>
2021-11-16 23:01:14 +0100

    feat: check screen resolution – X.org

--- tdbs/generate_wallpaper.sh
+++ tdbs/generate_wallpaper.sh
@@ -2,5 +2,12 @@
 python3 tex_input.py
 cd ../latex
 pdflatex background.tex >pdflatex.log
-pdftoppm -scale-to-x 1920 -scale-to-y 1080 background.pdf bc -png
+if [ -x "$(command -v xdpyinfo)" ]; then
+    width=`xdpyinfo | grep dimensions | awk '{print $2}' | awk -Fx '{print $1}'`
+    height=`xdpyinfo | grep dimensions | awk '{print $2}' | awk -Fx '{print $2}'`
+else
+    width=1920
+    heigh=1080
+fi
+pdftoppm -scale-to-x ${width} -scale-to-y ${height} background.pdf bc -png
 feh --bg-fill bc-1.png
