124420c4f4658ef63a919d5749106bc19d064a9c
td236 <dadela.tomasz@gmail.com>
2021-11-16 21:50:04 +0100

    feat: habits db tables, pdflatex.log

--- tdbs/create_database.py
+++ tdbs/create_database.py
@@ -7,36 +7,60 @@ con = sqlite3.connect(settings.DATABASE_PATH)
 try:
     with con:
         cur = con.cursor()
-        cur.execute("""
+        cur.executescript("""
             CREATE TABLE TASK (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 tex_id INTEGER NOT NULL,
                 name TEXT NOT NULL
             );
-        """)
 
-        cur.execute("""
             CREATE TABLE DONE (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL
             );
-        """)
 
-        cur.execute("""
             CREATE TABLE OLD_TASK (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL
             );
-        """)
-        cur.execute("""
+
             CREATE TABLE MIGRATION (
-                last_migration timestamp
+                last_migration timestamp NOT NULL
+            );
+
+            CREATE TABLE HABIT (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT DEFAULT "" NOT NULL,
+                created timestamp NOT NULL,
+                first_day timestamp,
+                last_day timestamp
+            );
+
+            CREATE TABLE HABIT_STATUS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT DEFAULT "" NOT NULL
+            );
+
+            CREATE TABLE HABIT_TRACKING (
+                habit_id INTEGER NOT NULL,
+                status_id INTEGER NOT NULL,
+                day timestamp,
+
+                CONSTRAINT fk_habit
+                FOREIGN KEY (habit_id)
+                REFERENCES HABIT_TRACKING (_id)
+
+                CONSTRAINT fk_status
+                FOREIGN KEY (status_id)
+                REFERENCES HABIT_TRACKING (id)
             );
         """)
         cur.execute("""
         INSERT INTO MIGRATION VALUES(CURRENT_TIMESTAMP);
         """)
-except (sqlite3.OperationalError, sqlite3.IntegrityError) as e:
-    print('Could not complete operation:', e)
+except (sqlite3.OperationalError, sqlite3.IntegrityError) as err:
+    print('Could not complete operation:', err)
 
 con.close()
--- tdbs/generate_wallpaper.sh
+++ tdbs/generate_wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3 tex_input.py
 cd ../latex
-pdflatex background.tex
+pdflatex background.tex >pdflatex.log
 pdftoppm -scale-to-x 1920 -scale-to-y 1080 background.pdf bc -png
 feh --bg-fill bc-1.png
